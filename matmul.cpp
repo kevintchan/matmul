5dafcd340d8ac1512033e41ea9ad2c30f5967bfa
kevin_thomas_chan <cs194-ad@hive25.CS.Berkeley.EDU>
2012-09-15 20:44:53 -0700

    switched loops

--- matmul.cpp
+++ matmul.cpp
@@ -10,9 +10,9 @@ void matmuld(double **a, double **b, double **c, int block_size) {
   for(int ii=0; ii<matrix_dimension; ii+=block_size) {
     for(int jj=0; jj<matrix_dimension; jj+=block_size) {
       for(int kk=0; kk<matrix_dimension ;kk+=block_size) {
-	for (int i=ii; i<ii+block_size && i<matrix_dimension; i++) {
-	  for (int j=jj; j<jj+block_size && j<matrix_dimension; j++) {
-	    for (int k=kk; k<kk+block_size && k<matrix_dimension; k+=4) {
+	for (int i=ii; i<ii+block_size; i++) {
+	  for (int k=kk; k<kk+block_size; k++) {
+	    for (int j=jj; j<jj+block_size; j++) {
 	      c[i][j] += a[i][k]*b[k][j];
 	    }
 	  }
